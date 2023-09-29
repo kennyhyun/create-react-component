167f0a951286e063f7c4cebc249b9c3e5dd49af2
Kenny Yeo <kenny@yeoyou.net>
2023-09-29 14:01:18 +0000

    fix: review eslint options

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,30 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   ignorePatterns: ['node_modules'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'react'],
+  overrides: [
+    {
+      files: ['.eslintrc.{js,cjs}', 'bin/**/*.{js,cjs,mjs}'],
+      env: {
+        node: true,
+      },
+      parser: 'espree',
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+    {
+      files: ['src/**/*.ts'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+    },
+  ],
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
   },
